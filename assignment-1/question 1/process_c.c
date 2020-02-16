66aef22d17b3e165f46aa7c2d1653d3ac8ba402b
Jeremy <jeremylumanbailey@gmail.com>
2020-02-16 16:54:07 -0500

    question 1, 3 and 4 pretty much done

---
+++ assignment-1/question 1/process_c.c
@@ -0,0 +1,119 @@
+
+/*
+ * shm-client - client program to demonstrate shared memory.
+ * shm-client - not sure of the origin of these code segments.
+ * possibly: David Marshalls course : http://www.cs.cf.ac.uk/Dave/C/CE.html
+ * or Steve Holmes : http://www2.its.strath.ac.uk/courses/c/
+ */
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SHMSZ     27
+
+int
+main()
+{
+    int shmid, shmid2;
+    key_t key,key2;
+    char *shm, *s, *sharedmem2,  *shared2;
+
+
+    /*
+     * We need to get the segment named
+     * "5678", created by the server.
+     */
+    key = 0511;
+
+
+
+    key2 = 0622;
+
+    /*
+     * Locate the segment.
+     */
+    if( (shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0 )
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+
+    /*
+     * Locate the second segment.
+     */
+    if( (shmid2 = shmget(key2, SHMSZ, IPC_CREAT | 0666)) < 0 )
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    /*
+     * Now we attach the segment to our data space.
+     */
+    if( (shm = shmat(shmid, NULL, 0)) == (char *) -1 )
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+
+    /*
+     * Now we attach the second segment to our data space.
+     */
+    if( (sharedmem2 = shmat(shmid2, NULL, 0)) == (char *) -1 )
+    {
+        perror("shmat");
+        exit(1);
+    }
+
+
+
+/***************************************************************************/
+
+    while( *shm != '2' )
+        sleep(1);
+
+        /*
+     * Now read what the server put in the memory.
+     */
+
+
+
+        for( s = shm; *s != (char) NULL; s++ )
+        putchar(*s);
+    putchar('\n');
+
+
+    shared2 = sharedmem2;
+
+    strcpy(shared2,"memory ");
+
+
+
+        for( shared2 = sharedmem2; *shared2 != (char) NULL; shared2++ )
+        putchar(*shared2);
+    putchar('\n');
+
+
+
+        /*
+     * Now read what the server put in the memory.
+     */
+
+
+    s = shm;
+    *s = '3';
+
+
+    /*
+     * Finally, change the first character of the
+     * segment to '*', indicating we have read
+     * the segment.
+     */
+
+    return 0;
+}
